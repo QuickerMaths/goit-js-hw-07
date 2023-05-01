676c7ae6e24c76cd5e74277123dd81e417fe9197
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-05-01 12:34:23 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const listEl = `<li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img
+          class="gallery__image"
+          src=${image.preview}
+          data-source=${image.original}
+          alt=${image.description}
+        />
+      </a>
+    </li>`;
+
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+  galleryEl.addEventListener("click", (e) => e.preventDefault());
+}
+
+galleryEl.addEventListener("click", (e) => {
+  const instance = basicLightbox.create(`<img src=${e.target.dataset.source}>`);
+  instance.show();
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const listEl = `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            class="gallery__image"
+            src=${image.preview}
+            data-source=${image.original}
+            alt=${image.description}
+          />
+        </a>
+      </li>`;
+
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+  galleryEl.addEventListener("click", (e) => e.preventDefault());
+}
+
+galleryEl.insertAdjacentHTML("beforeend", galleryEl);
+
+let lightbox = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox", () => {
+  galleryEl.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      lightbox.close();
+    }
+  });
+});
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+});
